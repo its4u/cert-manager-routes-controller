971f5bc6659c5ba1a01192d1e12969c4d08291f8
Alexandre Perlmutter <alexandre.perlmutter@gmail.com>
2023-07-11 11:59:31 +0200

    Create a context to use in `reconcile`

--- ctrl/src/main.rs
+++ ctrl/src/main.rs
@@ -1,4 +1,6 @@
 pub mod crd;
+pub mod manage_route;
+pub mod types;
 
 use std::{sync::Arc, time::Duration};
 use futures::StreamExt;
@@ -7,32 +9,35 @@ use kube::{
     runtime::controller::{Action, Controller}
 };
 use k8s_openapi::api::core::v1::Secret;
-use crd::certificate::Certificate;
 use crd::route::Route;
+use manage_route::*;
+use types::*;
 
+const DEFAULT_CERT_MANAGER_NAMESPACE: &'static str = "cert-manager";
+const CERT_MANAGER_NAMESPACE_ENV: &'static str = "CERT_MANAGER_NAMESPACE";
 
-#[derive(thiserror::Error, Debug)]
-pub enum Error {}
-pub type Result<T, E = Error> = std::result::Result<T, E>;
 
 #[tokio::main]
 async fn main() -> Result<(), kube::Error> {
-    let client = Client::try_default().await?;
-    let routes = Api::<Route>::all(client);
+    let cert_manager_namespace = std::env::var(CERT_MANAGER_NAMESPACE_ENV).unwrap_or(DEFAULT_CERT_MANAGER_NAMESPACE.to_owned());
+    let context = Arc::new(ContextData { 
+        client: Client::try_default().await?,
+        cert_manager_namespace,
+    });
 
-    Controller::new(routes.clone(), Default::default())
-        .run(reconcile, error_policy, Arc::new(()))
+    Controller::new(Api::<Route>::all(context.client.clone()), Default::default())
+        .run(reconcile, error_policy, context)
         .for_each(|_| futures::future::ready(()))
         .await;
 
     Ok(())
 }
 
-async fn reconcile(obj: Arc<Route>, ctx: Arc<()>) -> Result<Action> {
-    println!("reconcile request: {}", obj.name_any());
+async fn reconcile(obj: Arc<Route>, ctx: Arc<ContextData>) -> Result<Action> {
+    println!("reconcile request: {}\nAnnotations: {:?}",  obj.name_any(), obj.metadata.annotations);
     Ok(Action::requeue(Duration::from_secs(3600)))
 }
 
-fn error_policy(_object: Arc<Route>, _err: &Error, _ctx: Arc<()>) -> Action {
+fn error_policy(_object: Arc<Route>, _err: &Error, _ctx: Arc<ContextData>) -> Action {
     Action::requeue(Duration::from_secs(5))
 }
