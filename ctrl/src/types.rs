53f0f439d6e4e33a1f99edf92a1045c006943817
Alexandre Perlmutter <alexandre.perlmutter@gmail.com>
2023-07-12 11:18:30 +0200

    Implement `new` for `ContextData`

--- ctrl/src/types.rs
+++ ctrl/src/types.rs
@@ -1,8 +1,21 @@
+use kube::Client;
+
 #[derive(thiserror::Error, Debug)]
 pub enum Error {}
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
 pub struct ContextData {
-    pub client: kube::Client,
+    pub client: Client,
     pub cert_manager_namespace: String,
+    pub cert_annotation_key: String,
+}
+
+impl ContextData {
+    pub fn new(client: Client, cert_manager_namespace: String, cert_annotation_key: String) -> Self {
+        Self {
+            client,
+            cert_manager_namespace,
+            cert_annotation_key
+        }
+    }
 }
