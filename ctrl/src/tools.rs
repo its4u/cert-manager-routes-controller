c023ee991e7930843fff343f125bda873894e155
Alexandre Perlmutter <alexandre.perlmutter@gmail.com>
2023-07-14 09:05:33 +0200

    Document code #1

--- ctrl/src/tools.rs
+++ ctrl/src/tools.rs
@@ -3,14 +3,51 @@ use crate::route::{TLS_CRT, TLS_KEY};
 use crate::types::ContextData;
 use k8s_openapi::api::core::v1::Secret;
 use k8s_openapi::ByteString;
-use kube::{Api, ResourceExt};
+use kube::Api;
 use std::collections::BTreeMap;
 use std::collections::HashSet;
 
+/// Format a resource to a string in the format `namespace:name`.
+/// 
+/// ### Arguments
+/// 
+/// * `name` - The name of the resource.
+/// * `namespace` - The namespace of the resource.
+/// 
+/// ### Returns
+/// 
+/// A [`String`] containing the formatted resource.
+/// 
+/// ### Example
+/// 
+/// ```rust
+/// let resource = resource_to_string("world", "hello");
+/// println!("{}", resource); // hello:world
+/// ```
 pub fn resource_to_string(name: &str, namespace: &str) -> String {
     format!("{}:{}", namespace, name)
 }
 
+/// Format a [`Certificate`] annotation value in the format `namespace:name(,namespace:name)*`.
+/// 
+/// If the annotation doesn't exists yet, the annotation value will contain a single route. 
+/// Else, the route will be appended to the annotation value.
+/// 
+/// ### Arguments
+/// 
+/// * `cert_annotation` - The current annotation value.
+/// * `route` - The [`Route`] to append to the annotation value.
+/// 
+/// ### Returns
+/// 
+/// A [`String`] containing the formatted annotation value.
+/// 
+/// ### Example
+/// 
+/// ```rust
+/// let annotation = format_cert_annotation(None, &route);
+/// println!("{}", annotation);
+/// ```
 pub fn format_cert_annotation(cert_annotation: Option<&String>, route: &Route) -> String {
     match cert_annotation {
         Some(cert_annotation) => {
@@ -25,14 +62,63 @@ pub fn format_cert_annotation(cert_annotation: Option<&String>, route: &Route) -
     }
 }
 
+/// Format a [`Certificate`] name in the format `hostname-cert`.
+/// 
+/// ### Arguments
+/// 
+/// * `hostname` - The hostname of the [`Certificate`].
+/// 
+/// ### Returns
+/// 
+/// A [`String`] containing the formatted [`Certificate`] name.
+/// 
+/// ### Example
+/// 
+/// ```rust
+/// let cert_name = format_cert_name("example.com");
+/// println!("{}", cert_name); // example.com-cert
+/// ```
 pub fn format_cert_name(hostname: &str) -> String {
     format!("{}-cert", hostname)
 }
 
+/// Format a [`Secret`] name in the format `hostname-tls`.
+/// 
+/// ### Arguments
+/// 
+/// * `hostname` - The hostname of the [`Secret`].
+/// 
+/// ### Returns
+/// 
+/// A [`String`] containing the formatted [`Secret`] name.
+/// 
+/// ### Example
+/// 
+/// ```rust
+/// let cert_name = format_secret_name("example.com");
+/// println!("{}", cert_name); // example.com-tls
+/// ```
 pub fn format_secret_name(hostname: &str) -> String {
     format!("{}-tls", hostname)
 }
 
+/// Get the TLS data from a [`Secret`]'s [`Certificate`].
+/// 
+/// ### Arguments
+/// 
+/// * `cert_name` - The name of the [`Certificate`] to extract the TLS data from.
+/// * `ctx` - The [`ContextData`].
+/// 
+/// ### Returns
+/// 
+/// A [`Result`] containing a [`BTreeMap`] of the TLS data or a [`kube::Error`].
+/// 
+/// ### Example
+/// 
+/// ```rust
+/// let tls_data = get_secret_tls_data(&cert_name, &ctx).await?;
+/// println!("TLS data: {:?}", tls_data);
+/// ```
 pub async fn get_secret_tls_data(
     cert_name: &str,
     ctx: &ContextData,
