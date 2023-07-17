10e2193a092ea61e2653816806430f2079088461
Alexandre Perlmutter <alexandre.perlmutter@gmail.com>
2023-07-17 08:26:41 +0200

    Stop specifying the algorithm type for the pkey

--- src/certificate.rs
+++ src/certificate.rs
@@ -1,11 +1,10 @@
 use crate::crd::{
-    certificate::{
-        Certificate, CertificateIssuerRef, CertificatePrivateKey, CertificatePrivateKeyAlgorithm,
-        CertificateSpec,
-    },
+    certificate::{Certificate, CertificateIssuerRef, CertificateSpec},
     route::Route,
 };
-use crate::tools::{format_cert_annotation, format_cert_name, format_secret_name, resource_to_string};
+use crate::tools::{
+    format_cert_annotation, format_cert_name, format_secret_name, resource_to_string,
+};
 use crate::types::ContextData;
 use crate::{CERT_ANNOTATION_KEY, ISSUER_ANNOTATION_KEY};
 use kube::{
@@ -17,25 +16,30 @@ use std::fmt;
 
 impl Certificate {
     /// Create a new [`Certificate`] with some default values.
-    /// 
+    ///
     /// ### Arguments
-    /// 
+    ///
     /// * `name` - The name of the [`Certificate`].
     /// * `hostname` - The dnsName of the [`Certificate`].
     /// * `issuer_name` - The `ClusterIssuer` to use for the [`Certificate`]
     /// * `ctx` - The [`ContextData`].
-    /// 
+    ///
     /// ### Returns
-    /// 
+    ///
     /// A new [`Certificate`] instance.
-    /// 
+    ///
     /// ### Example
-    /// 
+    ///
     /// ```rust
     /// let cert = Certificate::new_default(&name, &hostname, &issuer_name, &ctx);
     /// println!("Created Certificate: {}", cert);
     /// ```
-    fn new_default(name: &String, hostname: &String, issuer_name: &String, ctx: &ContextData) -> Self {
+    fn new_default(
+        name: &String,
+        hostname: &String,
+        issuer_name: &String,
+        ctx: &ContextData,
+    ) -> Self {
         Certificate {
             status: None,
             metadata: ObjectMeta {
@@ -52,12 +56,7 @@ impl Certificate {
                     group: Some("cert-manager.io".to_owned()),
                 },
                 is_ca: Some(false),
-                private_key: Some(CertificatePrivateKey {
-                    algorithm: Some(CertificatePrivateKeyAlgorithm::Ecdsa),
-                    encoding: None,
-                    rotation_policy: None,
-                    size: Some(256),
-                }),
+                private_key: None,
                 additional_output_formats: None,
                 common_name: None,
                 duration: None,
@@ -72,36 +71,43 @@ impl Certificate {
                 subject: None,
                 uris: None,
                 usages: None,
-            }
+            },
         }
     }
 }
 
-/// Implement the [`fmt::Display`] trait for a [`Certificate`]. 
+/// Implement the [`fmt::Display`] trait for a [`Certificate`].
 /// It writes the data in [`resource_to_string()`] format.
 impl fmt::Display for Certificate {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", resource_to_string(self.metadata.name.as_ref().unwrap(), self.metadata.namespace.as_ref().unwrap()))
+        write!(
+            f,
+            "{}",
+            resource_to_string(
+                self.metadata.name.as_ref().unwrap(),
+                self.metadata.namespace.as_ref().unwrap()
+            )
+        )
     }
 }
 
 /// Annotate a [`Certificate`] with a [`Route`] that uses it.
-/// 
+///
 /// The annotation key is the one contained in [`CERT_ANNOTATION_KEY`].
 /// The annotation value is a comma-separated list of [`Route`]s each in the [`resource_to_string()`] format.
-/// 
+///
 /// ### Arguments
-/// 
+///
 /// * `cert_name` - The name of the [`Certificate`] to annotate.
 /// * `route` - The [`Route`] that uses the [`Certificate`].
 /// * `ctx` - The [`ContextData`].
-/// 
+///
 /// ### Returns
-/// 
+///
 /// A [`Result`] containing the annotated [`Certificate`] or a [`kube::Error`].
-/// 
+///
 /// ### Example
-/// 
+///
 /// ```rust
 /// let cert = annotate_cert(&cert_name, &route, &ctx).await?;
 /// println!("Annotated Certificate: {}", cert);
@@ -124,31 +130,29 @@ pub async fn annotate_cert(
         .patch(
             cert_name,
             &PatchParams::default(),
-            &Patch::Merge(
-                &serde_json::json!({
-                    "metadata": {
-                        "annotations": annotations,
-                    }
-                })
-            ),
+            &Patch::Merge(&serde_json::json!({
+                "metadata": {
+                    "annotations": annotations,
+                }
+            })),
         )
         .await?;
     Ok(cert)
 }
 
 /// Create a [`Certificate`] for a [`Route`]'s hostname.
-/// 
+///
 /// ### Arguments
-/// 
+///
 /// * `route` - The [`Route`] that will use the [`Certificate`].
 /// * `ctx` - The [`ContextData`].
-/// 
+///
 /// ### Returns
-/// 
+///
 /// A [`Result`] containing the created [`Certificate`] or a [`kube::Error`].
-/// 
+///
 /// ### Example
-/// 
+///
 /// ```rust
 /// let cert = create_certificate(&route, &ctx).await?;
 /// println!("Created Certificate: {}", cert);
@@ -162,23 +166,28 @@ pub async fn create_certificate(
     let cert_name = format_cert_name(&hostname);
     let cert_api: Api<Certificate> =
         Api::namespaced(ctx.client.clone(), &ctx.cert_manager_namespace);
-    let cert = Certificate::new_default(&cert_name, &hostname, &annotations.get(ISSUER_ANNOTATION_KEY).unwrap().to_owned(), &ctx);
+    let cert = Certificate::new_default(
+        &cert_name,
+        &hostname,
+        &annotations.get(ISSUER_ANNOTATION_KEY).unwrap().to_owned(),
+        &ctx,
+    );
     Ok(cert_api.create(&PostParams::default(), &cert).await?)
 }
 
 /// Check whether a [`Certificate`] exists.
-/// 
+///
 /// ### Arguments
-/// 
+///
 /// * `cert_name` - The name of the [`Certificate`] to check.
 /// * `ctx` - The [`ContextData`].
-/// 
+///
 /// ### Returns
-/// 
+///
 /// A [`bool`] indicating whether the [`Certificate`] exists.
-/// 
+///
 /// ### Example
-/// 
+///
 /// ```rust
 /// let exists = certificate_exists(&cert_name, &ctx).await;
 /// println!("Certificate `{}` exists: {}", &cert_name, exists);
@@ -194,19 +203,19 @@ pub async fn certificate_exists(cert_name: &str, ctx: &ContextData) -> bool {
 }
 
 /// Check whether a [`Certificate`] is annotated with a [`Route`].
-/// 
+///
 /// ### Arguments
-/// 
+///
 /// * `cert_name` - The name of the [`Certificate`] to check.
 /// * `route` - The [`Route`] to check in the annotation.
 /// * `ctx` - The [`ContextData`].
-/// 
+///
 /// ### Returns
-/// 
+///
 /// A [`Result`] containing a [`bool`] indicating whether the [`Certificate`] is annotated with the [`Route`] or a [`kube::Error`].
-/// 
+///
 /// ### Example
-/// 
+///
 /// ```rust
 /// let is_annotated = is_cert_annotated(&cert_name, &route, &ctx).await?;
 /// println!("Certificate `{}` is annotated with Route `{}`: {}", &cert_name, &route, is_annotated);
